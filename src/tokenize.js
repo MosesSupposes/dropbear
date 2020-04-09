1bf2566db616b789cd17c233900b10aacfcd6781
Moses A. Samuel <mosamuel98@gmail.com>
2020-04-08 18:19:21 -0700

    tokenize parenthesis, numbers, and letters

--- src/tokenize.js
+++ src/tokenize.js
@@ -6,6 +6,51 @@ const {
   isQuote,
 } = require('./identify');
 
-const tokenize = () => {};
+const tokenize = (input) => {
+  let tokens = [];
+  let cursor = 0;
+
+  while (cursor < input.length) {
+    let character = input[cursor];
+
+    if (isParenthesis(character)) {
+      tokens.push({
+        type: 'Parenthesis',
+        value: character,
+      });
+      cursor++;
+      continue;
+    }
+
+    if (isWhitespace(character)) {
+      cursor++;
+      continue;
+    }
+
+    if (isNumber(character)) {
+      tokens.push({
+        type: 'Number',
+        value: parseInt(character),
+      });
+
+      cursor++;
+      continue;
+    }
+
+    if (isLetter(character)) {
+      tokens.push({
+        type: 'Name',
+        value: character,
+      });
+
+      cursor++;
+      continue;
+    }
+
+    throw new Error(`${character} is not valid.`);
+  }
+
+  return tokens;
+};
 
 module.exports = { tokenize };
